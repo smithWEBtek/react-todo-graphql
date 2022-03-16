44a13196dd1533e9fcca8f9dae08284de5707ca9
brad@smithwebtek.com <brad@smithwebtek.com>
2022-03-15 21:21:31 -0400

    First commit

---
+++ endcode/src/components/CreatePost.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { PostContext } from "../App";
+
+function CreatePost({ user }) {
+  const { dispatch } = React.useContext(PostContext);
+  const [content, setContent] = React.useState("");
+  const [image, setImage] = React.useState(null);
+  const imageInputRef = React.useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const post = { content, image, user, id: Date.now() };
+    // handleAddPost(post);
+    dispatch({ type: "ADD_POST", payload: { post } });
+    setContent("");
+    setImage(null);
+    imageInputRef.current.value = "";
+  }
+
+  return (
+    <div>
+      <h1>Create New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Post Content"
+          onChange={event => setContent(event.target.value)}
+          value={content}
+        />
+        <input
+          type="file"
+          onChange={event => setImage(event.target.files[0])}
+          ref={imageInputRef}
+        />
+        <button type="submit">Submit Post</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePost;
